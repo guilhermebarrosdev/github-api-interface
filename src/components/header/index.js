bf694c5640706624c5f21c28a083b5d763175953
Guilherme Barros <wingui3@gmail.com>
2021-10-07 05:49:32 -0300

    :rocket: Finish

---
+++ src/components/header/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import * as S from "./styled";
+import useGithub from "../../hooks/github-hooks";
+
+const Header = () => {
+  const { getUser } = useGithub();
+  const [usernameForSearch, setUsernameForSearch] = useState();
+
+  const submitGetUser = () => {
+    if (!usernameForSearch) return;
+    return getUser(usernameForSearch);
+  };
+
+  document.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const btn = document.getElementById("btn");
+      btn.click();
+    }
+  });
+
+  return (
+    <header>
+      <S.Wrapper>
+        <input
+          type="text"
+          placeholder="Digite o username para pesquisa..."
+          onChange={(event) => setUsernameForSearch(event.target.value)}
+        />
+        <button id="btn" type="submit" onClick={submitGetUser}>
+          <span>Buscar</span>
+        </button>
+      </S.Wrapper>
+    </header>
+  );
+};
+
+export default Header;
